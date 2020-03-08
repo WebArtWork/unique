b0cde10776d51ddc8371cff69d81b83a1a66a906
Honchar Denys <crackeraki@gmail.com>
2020-03-09 01:08:29 +0200

    added mongoose connect, ignore

--- router.js
+++ router.js
@@ -1,5 +1,14 @@
 var Unique = require(__dirname+'/schema.js');
+var mongoose = require('mongoose');
 module.exports = function(sd) {
+	if(mongoose.connection.readyState==0){
+		mongoose.connect(sd.mongoUrl, {
+			useUnifiedTopology: true,
+			useNewUrlParser: true
+		});
+		mongoose.set('useCreateIndex', true);
+		mongoose.Promise = global.Promise;
+	}
 	sd.crud('unique', {
 		get: {
 			query: function() {
